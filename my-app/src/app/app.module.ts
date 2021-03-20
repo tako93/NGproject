35dda6e50a2e5aa1b54b5aba2f060a151ed7515c
Tako <73486278+tako93@users.noreply.github.com>
2021-03-20 19:51:26 +0400

    center search & logo

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
+import { CarsComponent } from './bmw/cars.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent
+    CarsComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
