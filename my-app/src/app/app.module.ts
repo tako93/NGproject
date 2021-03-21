3e64ee77b35bf35fc8c8199d042c2324d3fbe709
Tako <73486278+tako93@users.noreply.github.com>
2021-03-21 22:19:08 +0400

    modules

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,25 +4,18 @@ import { HttpClientModule } from '@angular/common/http'
 import { RouterModule } from '@angular/router'
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
-// import { CarsComponent } from './bmw/cars.component';
-import { UsersApiComponent } from './users-api/users-api.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { UsersCardComponent } from './users-api/users-card/users-card.component';
-import { UserDetailComponent } from './users-api/user-detail/user-detail.component';
-import { AuthGuard } from './auth.guard';
 import { BmwModule } from './bmw/bmw.module';
+import { UsersModule } from './users-api/users.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersApiComponent,
     PageNotFoundComponent,
     NavigationComponent,
-    UsersCardComponent,
-    UserDetailComponent,
-
   ],
   imports: [
     BrowserModule,
@@ -30,20 +23,13 @@ import { BmwModule } from './bmw/bmw.module';
     HttpClientModule,
     BmwModule,
     RouterModule.forRoot([
-      {
-        path: 'users',
-        component: UsersApiComponent,
-        canActivate: [AuthGuard]
-      },
-         {
-        path: 'user/:email',
-        component: UserDetailComponent,
-      },
         {
         path: '**',
         component: PageNotFoundComponent,
       }
     ]),
+    UsersModule,
+
     
   ],
   providers: [],
