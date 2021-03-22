7999d60d916a8376539a3c306f690bf7ffb4b29f
Tako <73486278+tako93@users.noreply.github.com>
2021-03-22 16:53:05 +0400

    pipeForError

--- my-app/src/app/users-api/user-resolver.service.ts
+++ my-app/src/app/users-api/user-resolver.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import {
-  Resolve,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
+    Resolve,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
 } from '@angular/router';
 
 import { IUsersData, IUsers } from './users-api';
 import { UsersService } from './users-api.service';
+import { catchError } from 'rxjs/operators';
 
 
 @Injectable({
@@ -18,10 +19,11 @@ import { UsersService } from './users-api.service';
 export class UserResolver implements Resolve<IUsers> {
 
     constructor(private _usersService: UsersService) { }
-    
+
     resolve(route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<IUsers> {
+        state: RouterStateSnapshot): Observable<IUsers> {
         return this._usersService.getUsers()
-    
+        .pipe(catchError((err) => of(err)))
+
     }
 }
