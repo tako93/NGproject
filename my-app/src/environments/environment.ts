59e2ef7abe3cd3fe4d065688ca83b5391f5a12a8
Tako <73486278+tako93@users.noreply.github.com>
2021-03-27 14:50:17 +0400

    fireConfig

--- my-app/src/environments/environment.ts
+++ my-app/src/environments/environment.ts
@@ -5,15 +5,7 @@
 export const environment = {
   production: false,
 
-   firebaseConfig: {
-    apiKey: "AIzaSyD4ETFOFWWYj4Lx-xU6U_bhx3jQObPg2Nc",
-    authDomain: "angular-demo-7bcd1.firebaseapp.com",
-    projectId: "angular-demo-7bcd1",
-    storageBucket: "angular-demo-7bcd1.appspot.com",
-    messagingSenderId: "449800680230",
-    appId: "1:449800680230:web:183a0eb241ea2028d307e7",
-    measurementId: "G-WPF5TCM3YC"
-  }
+
 };
 
 
