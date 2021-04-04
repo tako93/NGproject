c54edbc1a07beb8d9de94dd6d08d2a56f6822a19
Tako <73486278+tako93@users.noreply.github.com>
2021-04-04 12:14:38 +0400

    required sign

--- my-app/src/app/auth/sign-in/sign-in.component.ts
+++ my-app/src/app/auth/sign-in/sign-in.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-// import { Router } from '@angular/router';
-// import { LocalStorageService } from 'src/app/core/storage.service';
 import { SignInData } from 'src/app/data/sign-in-form.interface';
-// import { TOKEN_KEY } from 'src/app/auth/shared/constants';
-// import { AuthService } from '../shared/auth.service';
 import { FirebaseAuthService } from '../shared/firebase-auth.service';
 
 @Component({
@@ -13,15 +9,21 @@ import { FirebaseAuthService } from '../shared/firebase-auth.service';
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-  signInData: SignInData = {
-    email: 'takko.city@mail.com',
-    password: 'takotako',
+  // signInData: SignInData = {
+  //   email: 'takko.city@mail.com',
+  //   password: 'takotako',
+  //   remember: false,
+  // };
+
+  public email = '';
+
+   signInData: SignInData = {
+    email: '',
+    password: '',
     remember: false,
   };
+
   constructor(
-    // private router: Router,
-    // private _authService: AuthService,
-    // private storageService: LocalStorageService,
     private fireAuthService: FirebaseAuthService,
   ) {}
 
